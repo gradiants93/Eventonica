8b929e70a19bd24c73cd4af04ce3e08657931687
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2022-03-17 10:41:18 -0700

    WORKING added mockdata to show users route is working

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express is working properly" });
+  res.render("index", { title: "Express working properly" });
 });
 
 module.exports = router;
